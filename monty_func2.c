7af1d3fb8724332387e188a0262a6c4e3ae598ae
ayamorcito <izquierdoz.g@gmail.com>
2022-05-12 11:30:22 -0700

    addfix

--- monty_func2.c
+++ monty_func2.c
@@ -12,7 +12,7 @@ void _add(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: scan't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		error_exit(stack);
 	}
 
