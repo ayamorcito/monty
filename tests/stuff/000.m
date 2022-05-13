bdee9bb14cb451a753f00c8985d24092f34a0b21
Vinill <danimuller758@gmail.com>
2022-05-13 08:23:46 -0700

    Fixed div

--- tests/stuff/000.m
+++ tests/stuff/000.m
@@ -1,26 +1,2 @@
-push -1-2
-
-push 3 Push 4 onto the stack$
- 
-	
-	
-push 2 afdjvñkmfñsavlm
-
-  push 3
-
-                   pall    
-
-
-
-
-
-                           
-
-push 4
-
-
-
-    push 5    
-
-      push    6       
-     pall aksdmkmsd 
+push 2a
+pall
